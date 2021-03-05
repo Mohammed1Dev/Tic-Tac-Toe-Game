9deea84f58e4bceadfbfd2afd0fbf4e53a9b4987
Mohammed Bettaoui <bettapro100@gmail.com>
2021-03-05 14:53:12 +0100

    X-O Game Over

---
+++ src/components/Square.js
@@ -0,0 +1,27 @@
+
+
+function Square(props) {
+  return (
+        <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+  )
+  
+  
+}
+
+// class Square extends React.Component {
+
+//     render() {
+//       return (
+//         <button 
+//         className="square" 
+//         onClick={()=> this.props.onClick()}
+//         >
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+
+  export default Square
